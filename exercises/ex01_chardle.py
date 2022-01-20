c41d288552addf2ba55cfbeffefbe2b9b27c915a
Karli Meckler <kmeckler@live.unc.edu>
2022-01-20 16:21:44 -0500

    ex01_final_unchecked

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,44 +1,44 @@
-"""EX01: Chardle - Input, Variables, and Conditionals"""
-"""Wordle 1/20/21 is ROBOT."""
+"""EX01: Chardle - Input, Variables, and Conditionals."""
+# The Wordle for 1/20/21 is ROBOT.
 
 __author__ = "730231293"
 
-"""Part 1: Prompting for Inputs"""
-chardle: str = input("Enter a 5-character word")
-character: str = input("Enter a single character")
+chardle: str = input("Enter a 5-character word: ")
+instances: int = 0
+
+if len(chardle) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+character: str = input("Enter a single character: ")
+
+if len(character) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+
 print("Searching for " + character + " in " + chardle)
 
-
-"""Part 2: Checking Indices for Matches"""
-if chardle == "heels":
-    if character == "h": 
-        print("h found at index 0")
-    else:
-        if character == "e":
-            print("e found at index 1")
-            print("e found at index 2")
-        else:
-            if character == "l":
-                print("l found at index 3")
-            else: 
-                if character == "s":
-                    print("s found at index 4")
-                else:
-                    print("character not found")
+if chardle[0] == character:
+    print(character + " found at index 0")
+    instances = instances + 1
+if chardle[1] == character:
+    print(character + " found at index 1")
+    instances = instances + 1
+if chardle[2] == character:
+    print(character + " found at index 2")
+    instances = instances + 1
+if chardle[3] == character:
+    print(character + " found at index 3")
+    instances = instances + 1
+if chardle[4] == character:
+    print(character + " found at index 4")
+    instances = instances + 1
+if instances > 1:
+    print(str(instances) + " instances of " + character + " found in " + chardle)
 else: 
-    if chardle == "hello":
-        if character == "h": 
-            print("h found at index 0")
-        else:
-            if character == "e":
-                print("e found at index 1")
-            else:
-                if character == "l":
-                    print("l found at index 2")
-                    print("l found at index 3")
-                else: 
-                    print("character not found")
+    if instances == 1: 
+        print(str(instances) + " instance of " + character + " found in " + chardle)
     else: 
-        print("try another word (hint: 'hello' or 'heels'")
+        print("No instances of " + character + " found in " + chardle)
 
-"""Part 3: Counting Machine Indices"""
+ 
