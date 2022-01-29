0ca55bfca905afa670c65420434a9ed3ce7fc915
Karli Meckler <kmeckler@live.unc.edu>
2022-01-29 06:47:50 -0500

    ex02_final_1

--- exercises/ex02_wordle_loops.py
+++ exercises/ex02_wordle_loops.py
@@ -2,26 +2,37 @@
 
 __author__ = "730231293"
 
-secret: str = input("What is your 6-letter guess? ")
+wordle: str = "python"
+wordle_len: str = str(len(wordle))
+secret: str = input("What is your " + wordle_len + "-letter guess? ")
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
-wordle: str = "python"
-i = 0
+i: int = 0
 result = str = ""
 
-while len(secret) != 6:
-    secret = input("That was not 6 letters! Try again: ")
+
+while len(secret) != len(wordle):
+    secret = input("That was not " + wordle_len + " letters! Try again: ")
 
 if secret != wordle:
     while i < len(wordle):
         if secret[i] != wordle[i]:
-            result = result + white_box
-        else: 
-            result = result + green_box
+            j: int = 0
+            match_test: bool = False
+            while j < len(wordle):
+                if secret[i] == wordle[j]:
+                    match_test = True
+                j = j + 1
+            if match_test is True:
+                result += yellow_box
+            else:
+                result += white_box
+        else:
+            result += green_box
         i = i + 1
     print(result)
     print("Not quite. Play again soon!")
 else: 
-    print(green_box * 6)
+    print(green_box * len(wordle))
     print("Woo! You got it!")
